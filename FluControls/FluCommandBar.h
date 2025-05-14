d13742d717b4b587d4ff82915cf0379e1688d902
mowangshuying <1911364602@qq.com>
2025-05-14 08:03:21 +0800

    update.

---
+++ FluControls/FluCommandBar.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include <QFrame>
+#include "../FluUtils/FluUtils.h"
+
+class FluCommandBar : public QFrame 
+{
+	Q_OBJECT
+  public:
+	  FluCommandBar(QWidget* parent = nullptr) : QFrame(parent)
+	  {
+
+	  }
+   public slots:
+   protected:
+
+};

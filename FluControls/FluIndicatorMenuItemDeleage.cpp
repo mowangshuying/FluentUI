d811756d54d7e7d2cb10e0502693b91ace0bdcc8
mowangshuying <1911364602@qq.com>
2025-09-17 04:04:32 +0800

    fix FluRoundMenu see abnormal at AtomOneDarkTheme.

--- FluControls/FluIndicatorMenuItemDeleage.cpp
+++ FluControls/FluIndicatorMenuItemDeleage.cpp
@@ -1,4 +1,4 @@
-#include "FluIndicatorMenuItemDeleage.h"
+﻿#include "FluIndicatorMenuItemDeleage.h"
 #include "../FluUtils/FluUtils.h"
 
 FluIndicatorMenuItemDeleage::FluIndicatorMenuItemDeleage(QObject *parent /*= nullptr*/) : FluMenuItemDelegate(parent)
@@ -22,6 +22,9 @@ void FluIndicatorMenuItemDeleage::paint(QPainter *painter, const QStyleOptionVie
         painter->setBrush(QColor(0, 98, 158));
     else if (FluThemeUtils::isDarkTheme())
         painter->setBrush(QColor(118, 185, 237));
+    else if (FluThemeUtils::isAtomOneDarkTheme())
+        painter->setBrush(QColor(82, 139, 255));
+
 
     painter->drawRoundedRect(6, 9 + option.rect.y(), 3, 15, 1.5, 1.5);
     painter->restore();

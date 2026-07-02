016accf5edcd7123788154331d41169a5d6c9125
mowangshuying <1911364602@qq.com>
2026-07-02 18:32:08 +0800

    update.

--- Controls/FluPMenuPrivate.h
+++ Controls/FluPMenuPrivate.h
@@ -1,4 +1,22 @@
 #pragma once
-class FluPMenuPrivate
+
+#include <QObject>
+#include "FluPMenu.h"
+
+class FluPMenuPrivate : public QObject
 {
+	Q_OBJECT
+	Q_DECLARE_PUBLIC(FluPMenu)
+  public:
+	  FluPMenuPrivate(QObject* parent = nullptr) : QObject(parent)
+	  {
+
+	  }
+
+	  ~FluPMenuPrivate()
+	  {
+
+	  }
+  protected:
+    FluPMenu* q_ptr;
 };

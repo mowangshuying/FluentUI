8afcc8711b1278b7623566d59b02da82ab6f41e2
mowangshuying <1911364602@qq.com>
2026-07-22 17:19:35 +0800

    feat: add height animation to VNavigation indicator

--- Controls/FluVNavigationIndicator.cpp
+++ Controls/FluVNavigationIndicator.cpp
@@ -1,8 +1,12 @@
 #include "FluVNavigationIndicator.h"
 
-FluVNavigationIndicator::FluVNavigationIndicator(QWidget *parent /*= nullptr*/) : QWidget(parent), m_selected(false)
+FluVNavigationIndicator::FluVNavigationIndicator(QWidget *parent /*= nullptr*/) : QWidget(parent), m_selected(false), m_indicatorHeight(0), m_targetHeight(0)
 {
-    setFixedSize(3, 16);
+    setFixedWidth(3);
+    setMinimumHeight(0);
+    setMaximumHeight(16);
+    m_heightAnimation = new QPropertyAnimation(this, "indicatorHeight", this);
+    m_heightAnimation->setDuration(200);
 }
 
 bool FluVNavigationIndicator::isSelected() const
@@ -12,37 +16,49 @@ bool FluVNavigationIndicator::isSelected() const
 
 void FluVNavigationIndicator::setSelected(bool b)
 {
+    if (m_selected == b)
+        return;
     m_selected = b;
-    update();
+    m_heightAnimation->stop();
+    m_heightAnimation->setStartValue(m_indicatorHeight);
+    m_heightAnimation->setEndValue(b ? 16 : 0);
+    m_heightAnimation->start();
+}
+
+int FluVNavigationIndicator::indicatorHeight() const
+{
+    return m_indicatorHeight;
+}
+
+void FluVNavigationIndicator::setIndicatorHeight(int h)
+{
+    m_indicatorHeight = h;
+    setFixedHeight(h);
 }
 
 void FluVNavigationIndicator::paintEvent(QPaintEvent *event)
 {
     Q_UNUSED(event);
+    if (m_indicatorHeight == 0)
+        return;
+
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
-    if (m_selected)
-    {
-        if (FluThemeUtils::isLightTheme())
-        {
-            painter.setPen(Qt::NoPen);
-            painter.setBrush(QColor(0, 90, 158));
-        }
-        else if (FluThemeUtils::isDarkTheme())
-        {
-            painter.setPen(Qt::NoPen);
-            painter.setBrush(QColor(118, 185, 237));
-        }
-        else
-        {
-            painter.setPen(Qt::NoPen);
-            painter.setBrush(QColor(82, 139, 255));
-        }
-    }
+
+    QColor color;
+    if (FluThemeUtils::isLightTheme())
+        color = QColor(0, 90, 158);
+    else if (FluThemeUtils::isDarkTheme())
+        color = QColor(118, 185, 237);
     else
-    {
-        painter.setPen(Qt::NoPen);
-        painter.setBrush(Qt::transparent);
-    }
-    painter.drawRoundedRect(rect(), 1.5, 1.5);
+        color = QColor(82, 139, 255);
+
+    painter.setPen(Qt::NoPen);
+    painter.setBrush(color);
+
+    int radius = qMin(m_indicatorHeight / 2, 1);
+    QRect r = rect();
+    r.setTop((16 - m_indicatorHeight) / 2);
+    r.setHeight(m_indicatorHeight);
+    painter.drawRoundedRect(r, radius, radius);
 }
--- Controls/FluVNavigationIndicator.h
+++ Controls/FluVNavigationIndicator.h
@@ -2,21 +2,28 @@
 
 #include <QWidget>
 #include <QPainter>
+#include <QPropertyAnimation>
 #include "../Utils/FluThemeUtils.h"
 
 class FluVNavigationIndicator : public QWidget
 {
     Q_OBJECT
-    Q_PROPERTY(bool selected READ isSelected WRITE setSelected)
+    Q_PROPERTY(int indicatorHeight READ indicatorHeight WRITE setIndicatorHeight)
   public:
     FluVNavigationIndicator(QWidget *parent = nullptr);
 
     bool isSelected() const;
     void setSelected(bool b);
 
+    int indicatorHeight() const;
+    void setIndicatorHeight(int h);
+
   protected:
     void paintEvent(QPaintEvent *event) override;
 
   private:
     bool m_selected;
+    int m_indicatorHeight;
+    int m_targetHeight;
+    QPropertyAnimation *m_heightAnimation;
 };
